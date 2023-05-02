4d6876f8c24888027c166a47ca7ef4bc70a536a8
Alamin <alamin017514@gmail.com>
2023-05-02 11:58:06 +0600

    create alldata

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 const chefdetails = require('./data/chefdetails.json');
 
 app.get('/', (req, res) => {
